6517511061cce8ea89bdd74b4a6ba930f5f81817
sebastian2003 <tfkepper@gmail.com>
2024-06-03 14:00:30 +0300

    removed IDE dependencies

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/sebi/CLionProjects/PP_CPP
+# Install script for directory: /home/sebi/coding/c++_stuff/P2/ClasePP
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -50,5 +50,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/sebi/CLionProjects/PP_CPP/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/sebi/coding/c++_stuff/P2/ClasePP/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
